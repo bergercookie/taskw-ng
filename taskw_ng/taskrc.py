af172ef4dcd0acf7dac63d3fc6f1a089d9bc42a0
Nikos Koukis <nickkouk@gmail.com>
2024-01-22 12:29:45 +0200

    Attempt determining included rc file locations
    1. from the CWD (deprecated behavior in taskwarrior)
    2. in $TASK_RCDIR (custom path - just to allow flexibility from the end-user side
    3. relative to the TASKRC file
    4. in `{/usr/,/usr/local/}/share/doc/task/rc/`
    
    This fixes the taskw repo bug
    https://github.com/ralphbean/taskw/issues/150 . See also the relevant PR
    discussion in https://github.com/ralphbean/taskw/pull/151#

--- taskw_ng/taskrc.py
+++ taskw_ng/taskrc.py
@@ -86,6 +86,47 @@ class TaskRc(dict):
 
         return left
 
+    def _get_include_file_path(self, path: str) -> str:
+        if os.path.isabs(path):
+            return path
+
+        # search for this include file in a few pre-determined locations ------------------
+        # 1. from the CWD (deprecated behavior in taskwarrior)
+        # 2. in $TASK_RCDIR (custom path - just to allow flexibility from the end-user side
+        # 3. relative to the TASKRC file
+        # 4. in `{/usr/,/usr/local/}/share/doc/task/rc/`
+        if self.path is not None:
+            curr_path_rcdir = os.path.dirname(os.path.realpath(self.path))
+        else:
+            curr_path_rcdir = None
+
+        potential_rcdir_locations = [
+            dir_
+            for dir_ in [
+                os.path.realpath(os.path.curdir),
+                os.getenv("TASK_RCDIR"),
+                curr_path_rcdir,
+                "/usr/local/share/doc/task/rc/",
+                "/usr/share/doc/task/rc/",
+            ]
+            if dir_ is not None
+        ]
+
+        for rcdir in potential_rcdir_locations:
+            if rcdir is None:
+                continue
+
+            potential_abspath = f"{rcdir}/{path}"
+            if os.path.isfile(potential_abspath):
+                logger.debug(f"Found file to include at {potential_abspath} .")
+                return potential_abspath
+        else:
+            raise ValueError(
+                f"Cannot find location of included file - {path}. Looked for it in the"
+                f" following locations {potential_rcdir_locations}.\n\nYou can specify the"
+                " TASK_RCDIR env variable if you know the location of this file"
+            )
+
     def _read(self, path):
         config = {}
         with open(path, "r") as config_file:
@@ -96,11 +137,14 @@ class TaskRc(dict):
                 if line.startswith("include "):
                     try:
                         left, right = line.split(" ")
+                        right = self._get_include_file_path(right.strip())
                         config = self._merge_trees(config, TaskRc(right.strip()))
                     except ValueError:
                         logger.exception(
-                            "Error encountered while adding TaskRc at "
-                            "'%s' (from TaskRc file at '%s')",
+                            (
+                                "Error encountered while adding TaskRc at "
+                                "'%s' (from TaskRc file at '%s')"
+                            ),
                             right.strip(),
                             self.path,
                         )
@@ -112,8 +156,10 @@ class TaskRc(dict):
                         config = self._add_to_tree(config, key, value)
                     except ValueError:
                         logger.exception(
-                            "Error encountered while processing configuration "
-                            "setting '%s' (from TaskRc file at '%s')",
+                            (
+                                "Error encountered while processing configuration "
+                                "setting '%s' (from TaskRc file at '%s')"
+                            ),
                             line,
                             self.path,
                         )
