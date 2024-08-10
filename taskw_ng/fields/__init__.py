d77e0a86ad3ef89ffd0b20df64b9aa1999449f72
Nikos Koukis <nickkouk@gmail.com>
2024-08-10 12:57:09 +0300

    Add __all__ to taskw_ng/fields

--- taskw_ng/fields/__init__.py
+++ taskw_ng/fields/__init__.py
@@ -8,3 +8,16 @@ from .duration import DurationField
 from .numeric import NumericField
 from .string import StringField
 from .uuid import UUIDField
+
+__all__ = [
+    "AnnotationArrayField",
+    "ArrayField",
+    "Field",
+    "ChoiceField",
+    "CommaSeparatedUUIDField",
+    "DateField",
+    "DurationField",
+    "NumericField",
+    "StringField",
+    "UUIDField",
+]
